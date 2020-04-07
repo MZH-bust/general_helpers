4f2cf313cf5da70fb9b6988eb66ea5258f691d89
MZH-bust <martin.zoeltsch@bu-st-automotive.de>
2020-04-07 17:38:59 +0200

    Sharepoint Klasse zur Verarbeitung von Text mit Sharepoint Identifiern implementiert. Entsprechende Test Cases erstellt und erfolgreich getestet.

---
+++ general_helpers/sharepoint_helper.py
@@ -0,0 +1,94 @@
+import re
+
+
+class SpFieldText:
+    """
+    Möglichkeiten zur Verarbeitung von Informationen in Sharepoint Feldern.
+    z.B. Entfernen der Identifier aus dem Text oder Extraktion einer Liste der Identifier oder Werte aus einem Feld.
+
+    Attributes
+    ----------
+    field_text : str
+    identifier_value_list : list
+        Liste von Tupeln in der folgenden Form:
+        [
+        ("Identifier1", "Feldwert1),
+        ("Identifier2", "Feldwert2),
+        ...
+        ]
+    identifier_value_list_transponed : list
+        Liste von Tupeln in der folgenden Form:
+        [
+        ("Identifier1", "Identifier2, ...),
+        ("Feldwert1", "Feldwert2, ...)
+        ]
+
+    """
+    def __init__(self, field_text: str):
+        """
+
+        Parameters
+        ----------
+        field_text : str
+            Feldtext des Sharepoint-Felds
+        """
+        self.field_text = field_text
+        self.identifier_value_list = self._get_identifier_and_values_from_string()
+        self.identifier_value_list_transponed = self._transpone_identifier_value_list()
+
+    def _get_identifier_and_values_from_string(self):
+        """ Teilt den String "self.field_text" anhand der Sharepoint Identifier auf.
+
+        Returns
+        -------
+        list :
+            Gibt eine Liste von Tupeln in folgendem Format zurück:
+            [
+            ("Identifier1", "Feldwert1),
+            ("Identifier2", "Feldwert2),
+            ...
+            ]
+        """
+        # Erklärung zum regulären Ausdruck:
+        # (?:\d+;#|^) : Entweder (Eine oder mehrere Ziffern gefolgt von ";#") oder (Anfang des Strings)
+        # (.*?) : Beliebiger Text in der Mitte <-- Rückgabewert
+        # (?:;#|$) : Ende mit ";#" oder Zeilenende
+        return re.findall(r"(?:(\d+);#|^)(.*?)(?:;#|$)", self.field_text)
+
+    def _transpone_identifier_value_list(self):
+        """ Transponiert die Liste self.identifier_value_list
+
+        Returns
+        -------
+        list:
+            Gibt eine Liste von Tupeln in folgendem Format zurück:
+            [
+            ("Identifier1", "Identifier2, ...),
+            ("Feldwert1", "Feldwert2, ...)
+            ]
+        """
+        return [*zip(*self.identifier_value_list)]
+
+    def cleanse_text(self, delimiter: str = "; ") -> str:
+        """Gibt einen um die Sharepoint Identifier bereinigten Text zurück.
+
+        Parameters
+        ----------
+        delimiter : str
+            Trennzeichen, das zwischen den einzelnen Werten gesetzt werden soll.
+
+        Returns
+        -------
+        str:
+            Bereinigter Feldtext ohne die Sharepoint Identifier.
+
+        """
+        return delimiter.join(self.identifier_value_list_transponed[1])
+
+    def get_identifier_list(self) -> list:
+        """ Gibt eine Liste aller Sharepoint Identifier des Felds zurück"""
+        return list(self.identifier_value_list_transponed[0])
+
+    def get_value_list(self) -> list:
+        """ Gibt eine Liste aller Sharepoint Werte des Felds zurück"""
+        return list(self.identifier_value_list_transponed[1])
---
+++ tests/test_sharepoint_helper.py
@@ -0,0 +1,32 @@
+import pytest
+from general_helpers import sharepoint_helper
+
+
+class TestSpFieldText:
+    @pytest.mark.parametrize(
+        "test_parameter,expected",
+        [
+            pytest.param("20;#Lastname1, Firstname1;#91;#Lastname2, Firstname2;#184;#Lastname3, Firstname3",
+                         ("Lastname1, Firstname1; Lastname2, Firstname2; Lastname3, Firstname3",
+                          ["20", "91", "184"],
+                          ["Lastname1, Firstname1", "Lastname2, Firstname2", "Lastname3, Firstname3"]),
+                         id="Test for Multi User"),
+            pytest.param("61;#Lastname, Firstname",
+                         ("Lastname, Firstname",
+                          ["61"],
+                          ["Lastname, Firstname"]),
+                         id="Test for Single User with identifier"),
+            pytest.param("Lastname, Firstname",
+                         ("Lastname, Firstname",
+                          [""],
+                          ["Lastname, Firstname"]),
+                         id="Test for Single User without identifier"),
+
+        ],
+    )
+    def test_sort_alphanumeric(self, test_parameter, expected):
+        assert sharepoint_helper.SpFieldText(test_parameter).cleanse_text() == expected[0]
+        assert sharepoint_helper.SpFieldText(test_parameter).get_identifier_list() == expected[1]
+        assert sharepoint_helper.SpFieldText(test_parameter).get_value_list() == expected[2]
+        assert sharepoint_helper.SpFieldText(test_parameter).identifier_value_list_transponed == [tuple(expected[1]),
+                                                                                                  tuple(expected[2])]
